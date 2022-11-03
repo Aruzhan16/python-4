26c39fce867d3a695dc2733736d7bb360c70ad21
Aruzhan <100088756+Aruzhan16@users.noreply.github.com>
2022-11-03 20:49:21 +0600

    Update python.py

--- python.py
+++ python.py
@@ -1,178 +1,99 @@
-import collections
-import bcrypt
-from time import sleep
-from json2html import * 
-import requests, psycopg2, json
-from flask import Flask, render_template, request, redirect, url_for
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_wtf import FlaskForm
-from flask_bootstrap import Bootstrap
+from contextlib import _RedirectStream 
+from sqlite3 import Cursor
+from flask import Flask, render_template, request,url_for, redirect
+from flask import Flask
+from flask import request
+import psycopg2
+import requests
 
-# Connection to DB
-conn = psycopg2.connect(database="dbpyton", user = "postgres", password = "212552", host = "127.0.0.1", port = "5432")
+class User(object):
+    def __init__(self, name, status):
+        self.name = name
+        self.status = status
+    def getStatus(self):
+        return self.status
+    def setStatus(self, status):
+        self.status = status
+    def setName(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def logIn(self):
+        self.status = True
+    def logOut(self):
+        self.status = False
 
-url = "https://solana-gateway.moralis.io/nft/mainnet/{}/metadata"
-
-headers = {
-    "accept": "application/json",
-    "X-API-Key": "YOUR OWN API-KEY"
-}
-
+user = User('username', False)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Your_secret_key'
-bootstrap = Bootstrap(app)
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
-    remember = BooleanField('remember me')
+db = psycopg2.connect(dbname='dbpython', user='postgres', password='212552', host='localhost')
+cur = db.cursor()
 
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=4, max=15)])
+@app.route('/index') 
+def index(): 
+    if user.getStatus():
+        return render_template("index.html") 
+    else:
+        return redirect(url_for('login'), 302)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
+
+@app.route('/login', methods = ['POST', "GET"]) 
+def login(): 
+    user.logOut()
     if request.method == 'POST':
-        address = request.form.get('address')
+        username = request.form['username']
+        password = request.form['password']
+        cur.execute('SELECT * FROM users')
+        rows = cur.fetchall()
+        for row in rows:
+            if row[0] == username and row[1] == password:
+                user.logIn()
+                user.setName(username)
+                return redirect(url_for('index'), 301)
+        return render_template('login.html')
+    return render_template('login.html') 
+ 
+@app.route("/registration", methods = ['POST', "GET"])
+def registration():
+    user.logOut()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur.execute('INSERT INTO users (name, pass)'
+                    'VALUES (%s, %s)',
+                    (username, password))
+        db.commit()
+        return redirect(url_for('login'), 302)
+    return render_template('reg.html')
 
-        # Checking if the nft table exists in our database
-        cur = conn.cursor()
-        cur.execute("select * from information_schema.tables where table_name='nft_table'")
-        check1 = bool(cur.rowcount)
-        check2 = False
-        # Checking if our table has this particular address
-        if check1:
-            cur.execute("SELECT mint FROM nft_table WHERE mint = %s", (address,))
-            check2 = (cur.fetchone() is not None)   
+@app.route('/nft', methods=['POST', 'GET']) 
+def nft(): 
+    if user.getStatus():
+        output = request.form.to_dict()
+        adress = output["adress"]
+        headers = {
 
-        # If this address is already in our table, let's try to display it
-        if check2:
+            "accept": "application/json",
+            "X-API-Key": "SWnpmagdLrYt67aFhsaBRRzoubD59cdQkydkZLeljvVREBpWGmpLktfRLZXcvudp"
 
-            # The code below converts our record from the database to JSON, similar to what we get from Moralis API
-            cur.execute('''SELECT * FROM nft_table LEFT JOIN multiplex_table USING(metaplex_id) 
-                LEFT JOIN owners ON nft_table.mint = owners.nft_address WHERE mint=%s''', (address,))
-            rows = cur.fetchall()
-
-            owners = []
-            for owner in rows:
-                o = collections.OrderedDict()
-                o["address"] = owner[11]
-                o["share"] = owner[14]
-                o["verified"] = owner[13]
-                owners.append(o)
-
-            m = collections.OrderedDict()
-            m["isMutable"] = rows[0][9]
-            m["masterEdition"] = rows[0][10]
-            m["metadatauri"] = rows[0][5]
-            m["owners"] = owners
-            m["primarySaleHappened"] = rows[0][8]
-            m["sellerFeeBasisPoints"] = rows[0][7]
-            m["updateauthority"] = rows[0][6]
-            
-            n = collections.OrderedDict()
-            n["mint"] = rows[0][1]
-            n["name"] = rows[0][2]
-            n["standard"] = rows[0][3]
-            n["symbol"] = rows[0][4]
-            n['metaplex'] = m
-
-            j = json.dumps(n)
-            return render_template('result.html', table = json2html.convert(json = j))
-        # If nft address is not in our table, try to get it through Moralis API
+        }
+        url = 'https://solana-gateway.moralis.io/nft/mainnet/' + adress + '/metadata'
+        
+        
+        cur.execute("SELECT * FROM address WHERE addres = %s", (adress,))
+        ans = cur.fetchall()
+        response = requests.get(url, headers=headers)
+        print(ans)
+        if ans != []:
+            print('from db')
+            return render_template("result.html", result = response.text) 
         else:
-            response = requests.get(url.format(address), headers=headers).json()
-            sleep(3)
-
-            # In case of Bad Request
-            if 'statusCode' in response:
-                return '''<h1>NFT with this address not found</h1>'''
-
-            # If the Bad Request did not occur, Moralis will return JSON with information about the NFT 
-            # The code below saves this information to the DB
-            with conn.cursor() as cur:
-                cur.execute(""" CREATE TABLE IF NOT EXISTS owners(
-                    owner_address text, nft_address text, verified integer, share integer, PRIMARY KEY (owner_address, nft_address)) """)
-                query_sql = """ INSERT INTO owners(owner_address, nft_address, verified, share) VALUES(%s, %s, %s, %s) """
-                for o in response['metaplex']['owners']:
-                    cur.execute(query_sql, (o['address'], response['mint'], o['verified'], o['share'],))
-                conn.commit()
-
-                cur.execute(""" CREATE TABLE IF NOT EXISTS multiplex_table(
-                    metaplex_id serial, metadataUri text, updateAuthority text, sellerFeeBasisPoints integer, primarySaleHappened integer, 
-                    isMutable boolean, masterEdition boolean, PRIMARY KEY (metaplex_id)) """)
-                query_sql = """ INSERT INTO multiplex_table(metadataUri, updateAuthority, sellerFeeBasisPoints, primarySaleHappened, 
-                    isMutable, masterEdition) VALUES(%s, %s, %s, %s, %s, %s) """
-                cur.execute(query_sql, (response['metaplex']['metadataUri'], response['metaplex']['updateAuthority'], 
-                    response['metaplex']['sellerFeeBasisPoints'], response['metaplex']['primarySaleHappened'],
-                    response['metaplex']['isMutable'], response['metaplex']['masterEdition'],))
-                conn.commit() 
-
-                cur.execute(""" CREATE TABLE IF NOT EXISTS nft_table(
-                    mint text, name text, standard text, symbol text, metaplex_id serial, PRIMARY KEY (mint),
-                    FOREIGN KEY (metaplex_id) REFERENCES multiplex_table(metaplex_id)) """)
-                query_sql = """ INSERT INTO nft_table(mint, name, standard, symbol) VALUES(%s, %s, %s, %s) """ 
-                cur.execute(query_sql, (response['mint'], response['name'], response['standard'], response['symbol'],))
-                conn.commit()
-            
-            return render_template('result.html', table = json2html.convert(json = response))
-
-    return render_template('index.html')  
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        remember = form.remember.data
-
-        # Checking if the Users table exists in our database
-        cur = conn.cursor()
-        cur.execute("select * from information_schema.tables where table_name='users'")
-        check1 = bool(cur.rowcount)
-        check2 = False
-        # Checking if our table has this particular user
-        if check1:
-            cur.execute("SELECT name FROM users WHERE name = %s", (username,))
-            check2 = (cur.fetchone() is not None)   
-
-        # If this user is already in our table
-        if check2:
-            cur.execute('''SELECT * FROM users WHERE name=%s''', (username,))
-            user = cur.fetchone()
-
-            salt = bytes(user[2], 'utf-8')
-            if bcrypt.checkpw(password.encode('utf-8'), salt):
-                return redirect(url_for('index'))
-
-    return render_template('login.html', form=form)
-
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        username = form.username.data
-        email = form.email.data
-        password = form.password.data      
-
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-        with conn.cursor() as cur:
-            cur.execute(""" CREATE TABLE IF NOT EXISTS users(
-                name text, email text, pass text, PRIMARY KEY (name)) """)
-            query_sql = """ INSERT INTO users(name, email, pass) VALUES(%s, %s, %s) """
-            cur.execute(query_sql, (username, email, hashed_password,))
-            conn.commit()
-
-        return redirect(url_for('login'))
-
-    return render_template('signup.html', form=form)
+            cur.execute("INSERT INTO address (addres, info) VALUES (%s, %s)", (adress, response.text))
+            db.commit()
+            print('to db')
+            return render_template("result.html", result = response.text)
+    else:
+        return redirect(url_for('login'), 302)
 
 if __name__ == '__main__':
     app.run(debug=True)
